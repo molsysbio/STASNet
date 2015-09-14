921e47fa847800625a9c5bebc524943e06ef5624
Mathurin Dorel <dorel@horus.ens.fr>
2015-09-14 15:28:16 +0200

    Update documentation with roxygen2 4.1.1

---
+++ R/fitmodel.R
@@ -0,0 +1,10 @@
+#' @title Fit and simulate model from perturbation data
+#'
+#' @description Fits and simulates network model derived from perturbation data using Modular Response Analysis and Profile Likelihood
+#'
+#' @author Bertram Klinger \email{bertram.klinger@@charite.de}, Mathurin Dorel \email{dorel@@horus.ens.fr}, Nils Blüthgen \email{nils.bluethgen@@charite.de}
+#'
+#' @docType package
+#' @name fitmodel
+#' @aliases fitmodel-package
+NULL
