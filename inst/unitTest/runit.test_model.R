5e1e607c86e4f60fdf1b0ca8bb62d21957e9c806
Nils Blüthgen <nils@Nilss-MBP.fritz.box>
2015-11-09 07:17:57 +0100

    changed dirs in testsuite

--- inst/unitTest/runit.test_model.R
+++ inst/unitTest/runit.test_model.R
@@ -6,9 +6,9 @@ require(fitmodel)
 
 test.model.regression <- function() {
   
-  model=createModel("../inst/unitTest/adjacency.tab",
-    "../inst/unitTest/MIDAS.csv",
-    "../inst/unitTest/basal_activity.dat",inits=1000,method="correlation")
+  model=createModel("inst/unitTest/adjacency.tab",
+    "inst/unitTest/MIDAS.csv",
+    "inst/unitTest/basal_activity.dat",inits=1000,method="correlation")
 
   checkEquals(length(model$parameters), 2)
 }
@@ -18,8 +18,8 @@ test.model.regression <- function() {
 
 test.model.random <- function() {
   
-  model=createModel("../inst/unitTest/adjacency.tab",
-  "../inst/unitTest/MIDAS.csv","../inst/unitTest/basal_activity.dat",
+  model=createModel("inst/unitTest/adjacency.tab",
+  "inst/unitTest/MIDAS.csv","inst/unitTest/basal_activity.dat",
   inits=1000,method="random")
 
   checkEquals(length(model$parameters), 6)
--- tests/doSvUnit.R
+++ tests/doSvUnit.R
@@ -5,7 +5,7 @@ pkg <- "fitmodel"
 require(fitmodel)
 
 unlink("report.xml")  # Make sure we generate a new report
-mypkgSuite <- svSuiteList(pkg, dirs="../inst/unitTest")  # List all our test suites
+mypkgSuite <- svSuiteList(pkg, dirs="inst/unitTest")  # List all our test suites
 runTest(mypkgSuite, name = pkg)  # Run them...
 
 protocol(Log(), type = "junit", file = "report.xml")  # ... and write report
