51d2938deb4d2a3315258792a58500aa3abe311f
Mathurin Dorel <dorel@horus.ens.fr>
2016-01-29 15:36:57 +0100

    Improve package base documentation
    Make the base documentation displayed when using ?fitmodel more explicit
    and more helpful

--- R/fitmodel.R
+++ R/fitmodel.R
@@ -1,10 +1,16 @@
 #' @title Fit and simulate model from perturbation data
 #'
 #' @description Fits and simulates network model derived from perturbation data using Modular Response Analysis and Profile Likelihood
+#' @details This package provides functions to fit, simulate and examine a model network from perturbation data. It uses the Modular Response Analysis method described in "Network quantification of EGFR signaling unveils potential for targeted combination therapy, Klinger et al." to build the model.
+#'
+#' For a complete list of functions and classes, use 'library(help=fitmodel)'.
+#'
+#' For a walk-through as to how the package can be used to fit a model, see the vignette.
 #'
 #' @author Bertram Klinger \email{bertram.klinger@@charite.de}, Mathurin Dorel \email{dorel@@horus.ens.fr}, Nils Blüthgen \email{nils.bluethgen@@charite.de}
 #'
 #' @docType package
 #' @name fitmodel
 #' @aliases fitmodel-package
+#' @references Klinger et al. (2013) Network quantification of EGFR signaling unveils potential for targeted combination therapy. Molecular Systems Biology.
 NULL
