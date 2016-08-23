molsysbio/STASNet

commit 07b5a4eac91d426110eb06d570535f9d6dfaaf49

Update description and createModel doc for 'R CMD check'
Update DESCRIPTION to have Mathurin as corresponding author and complete
affiliation.
Remove examples in createModel that made 'R CMD check' raise an error.

DESCRIPTION
@@ -5,8 +5,8 @@ Version: 0.9
 Date: 2016-08-01
 Authors@R: c(person("Bertram", "Klinger", role=c("aut"),
     email="bertram.klinger@charite.de"), person("Mathurin", "Dorel",
-    role=c("aut"), email="dorel@horus.ens.fr"), person("Nils", "Bluethgen",
-    role=c("aut", "cre"), email="nils.bluethgen@charite.de"))
+    role=c("aut", "cre"), email="dorel@horus.ens.fr"), person("Nils", "Bluethgen",
+    role=c("aut", "cph", "ths"), email="nils.bluethgen@charite.de"))
 Description: Fits and simulates network model derived from perturbation data
     using Modular Response Analysis and Profile Likelihood
 License: GPL-3

R/create_model.R
@@ -50,10 +50,11 @@ get_running_time <- function(init_time, text="") {
 #' @seealso importModel, exportModel, rebuildModel
 #' @author Mathurin Dorel \email{dorel@@horus.ens.fr}
 #' @examples
-#' model = createModel("links.tab", "basal.dat", "data_MIDAS.csv") # Produces a model for the network described in links.tab using the data in data_MIDAS.csv
-#' model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", "variation.var") # Uses the variation from a variation file
-#' model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", nb_cores = detectCores()) # Uses all cores available (with the package parallel)
-#' model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", inits = 1000000) # Uses more initialisations for a complex network
+# model = createModel("links.tab", "basal.dat", "data_MIDAS.csv") # Produces a model for the network described in links.tab using the data in data_MIDAS.csv
+# model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", "variation.var") # Uses the variation from a variation file
+# model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", nb_cores = detectCores()) # Uses all cores available (with the package parallel)
+# model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", inits = 1000000) # Uses more initialisations for a complex network
+# TODO completely remove examples or add datafile so they work (or use data matrices)
 createModel <- function(model_links, basal_file, data.stimulation, data.variation="", nb_cores=1, inits=1000, perform_plots=F, precorrelate=T, method="geneticlhs", unused_perturbations=c(), unused_readouts=c()) {
   # Creation of the model structure object
   model_structure = extractStructure(model_links)

man/createModel.Rd
@@ -38,12 +38,6 @@ An MRAmodel object describing the model and its best fit, containing the data
 \description{
 Creates a parameterised model from experiment files and the network structure, and fit the parameters to the data
 }
-\examples{
-model = createModel("links.tab", "basal.dat", "data_MIDAS.csv") # Produces a model for the network described in links.tab using the data in data_MIDAS.csv
-model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", "variation.var") # Uses the variation from a variation file
-model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", nb_cores = detectCores()) # Uses all cores available (with the package parallel)
-model = createModel("links.tab", "basal.dat", "data_MIDAS.csv", inits = 1000000) # Uses more initialisations for a complex network
-}
 \author{
 Mathurin Dorel \email{dorel@horus.ens.fr}
 }

commit 0585f0eba85a4a27f5a5b14451b115c6acf9ce8c

Fix MRAmodelSet that was broken by 07b5a4eac91d426110eb06d570535f9d6dfaaf49
'name' instead of 'names' for compatibility with the MRAmodel constructor.
Fix email address in DESCRIPTION.

DESCRIPTION
@@ -5,7 +5,7 @@ Version: 0.9
 Date: 2016-08-01
 Authors@R: c(person("Bertram", "Klinger", role=c("aut"),
     email="bertram.klinger@charite.de"), person("Mathurin", "Dorel",
-    role=c("aut", "cre"), email="dorel@horus.ens.fr"), person("Nils", "Bluethgen",
+    role=c("aut", "cre"), email="mathurin.dorel@charite.de"), person("Nils", "Bluethgen",
     role=c("aut", "cph", "ths"), email="nils.bluethgen@charite.de"))
 Description: Fits and simulates network model derived from perturbation data
     using Modular Response Analysis and Profile Likelihood.

R/model_set.R
@@ -57,8 +57,8 @@ compareModels <- function(files) {
 #' @seealso \code{\link{createModel}}
 #' @author Mathurin Dorel \email{dorel@@horus.ens.fr}
 MRAmodelSet <- function(nb_models=1, model=NULL, design=NULL, structure=NULL, basal=matrix(), data=matrix(), cv=matrix(), parameters=vector(), bestfit=NA, name=c(), infos=c(), param_range=list(), lower_values=c(), upper_values=c(), unused_perturbations=c(), unused_readouts=c()) {
-    if (length(names) != nb_models) {
-        names = rep(names[1], nb_models)
+    if (length(name) != nb_models) {
+        name = rep(name[1], nb_models)
     }
 
     # An MRAmodelSet is an MRAmodel